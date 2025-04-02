5fb0292fbc054be27cc8053c3b2585efc47ace24
Jaxson Deppen <jaxsondeppen@mail.weber.edu>
2025-04-01 19:21:46 -0600

    test

--- openspace/urls.py
+++ openspace/urls.py
@@ -17,5 +17,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
-    path('event', include("openapp.urls"))
+    path('', include("openapp.urls"))
 ]
