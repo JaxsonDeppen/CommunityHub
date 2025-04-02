bbb5107ef9195be7fdd842163374c820c8bd197f
Jaxson Deppen <jaxsondeppen@mail.weber.edu>
2025-04-01 18:46:22 -0600

    close maybe

--- openspace/openspace/settings.py
+++ openspace/openspace/settings.py
@@ -116,15 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / 'assets']
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
